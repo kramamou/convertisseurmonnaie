3265c5b1e7f3b204dabbf4113944cf1ca396f101
Kevin RAMAMOURTHY <kevin.ramamourthy@gmail.com>
2015-11-21 15:14:44 +0100

    arborescense

---
+++ src/main/java/gui/MainController.java
@@ -0,0 +1,7 @@
+package gui;
+
+/**
+ * Created by Kevn on 21/11/2015.
+ */
+public class MainController {
+}
---
+++ src/main/java/model/index.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Kevn on 21/11/2015.
+ */
+public class index {
+}
